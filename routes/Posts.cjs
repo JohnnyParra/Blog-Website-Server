2e4094370e8afbda4fd1fdee22cb3fdd9889c507
JohnnyParra <juanparra1996@gmail.com>
2023-04-12 10:00:37 -0700

    ADDED featured route, UPDATED posts route

--- routes/Posts.cjs
+++ routes/Posts.cjs
@@ -1,5 +1,32 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
+
+router.get('/featured/:category', async (req, res) => {
+  const category = req.params.category;
+
+  try {
+    if (category == 0) {
+      const [post] = await req.db.query(`
+        SELECT * FROM posts
+        WHERE published = 1 AND deleted = 0
+        ORDER BY posts.likes DESC
+        LIMIT 1`
+      );
+      res.json({ post });
+    } else {
+      const [post] = await req.db.query(`
+        SELECT * FROM posts
+        WHERE category = ${category} AND published = 1 AND deleted = 0
+        ORDER BY posts.likes DESC
+        LIMIT 1`
+      );
+      res.json({ post });
+    }
+  } catch (err) {
+    console.log(err);
+    res.json({ err });
+  }
+});
 
 router.get('/:category/:sort', async (req, res) => {
   const category = req.params.category;
@@ -7,10 +34,16 @@ router.get('/:category/:sort', async (req, res) => {
 
   try {
     if(category == 0){
+      const [featuredPost] = await req.db.query(`
+        SELECT post_id FROM posts
+        WHERE published = 1 AND deleted = 0
+        ORDER BY posts.likes DESC
+        LIMIT 1`
+      );
       if(sort == 1){
         const [posts] = await req.db.query(`
           SELECT * FROM posts
-          WHERE published = 1 AND deleted = 0
+          WHERE post_id != '${featuredPost[0].post_id}' AND published = 1 AND deleted = 0
           ORDER BY date_created DESC
           LIMIT 3`
         );
@@ -18,7 +51,7 @@ router.get('/:category/:sort', async (req, res) => {
       } else if(sort == 2){
         const [posts] = await req.db.query(`
           SELECT * FROM posts
-          WHERE published = 1 AND deleted = 0
+          WHERE post_id != '${featuredPost[0].post_id}' AND published = 1 AND deleted = 0
           ORDER BY date_created DESC
           LIMIT 3`
         );
@@ -28,15 +61,21 @@ router.get('/:category/:sort', async (req, res) => {
           SELECT * FROM posts
           WHERE published = 1 AND deleted = 0
           ORDER BY posts.likes DESC
-          LIMIT 3`
+          LIMIT 1, 3`
         );
         res.json({ posts });
       }
     } else {
+      const [featuredPost] = await req.db.query(`
+        SELECT post_id FROM posts
+        WHERE category = ${category} AND published = 1 AND deleted = 0
+        ORDER BY posts.likes DESC
+        LIMIT 1`
+      );
       if(sort == 1){
         const [posts] = await req.db.query(`
           SELECT * FROM posts
-          WHERE category = ${category} AND published = 1 AND deleted = 0
+          WHERE category = ${category} AND post_id != '${featuredPost[0].post_id}' AND published = 1 AND deleted = 0
           ORDER BY date_created DESC
           LIMIT 2`
         );
@@ -44,7 +83,7 @@ router.get('/:category/:sort', async (req, res) => {
       } else if(sort == 2){
         const [posts] = await req.db.query(`
           SELECT * FROM posts
-          WHERE category = ${category} AND published = 1 AND deleted = 0
+          WHERE category = ${category} AND post_id != '${featuredPost[0].post_id}' AND published = 1 AND deleted = 0
           ORDER BY date_created DESC
           LIMIT 2`
         );
@@ -54,7 +93,7 @@ router.get('/:category/:sort', async (req, res) => {
           SELECT * FROM posts
           WHERE category = ${category} AND published = 1 AND deleted = 0
           ORDER BY posts.likes DESC
-          LIMIT 2`
+          LIMIT 1, 2`
         );
         res.json({ posts });
       }
