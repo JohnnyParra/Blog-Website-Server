79fa7f44541be215275f598dd26f447e280ce5e1
Johnny Parra <juanparra1996@gmail.com>
2024-05-17 15:56:26 -0700

    Profile to Vercel complete

--- routes/Profile.cjs
+++ routes/Profile.cjs
@@ -1,18 +1,10 @@
 const jwt = require('jsonwebtoken');
 const express = require("express");
-const { put } = require("@vercel/blob")
+const { put, del } = require("@vercel/blob")
 const multer = require('multer');
-const fs = require('fs');
 const bcrypt = require('bcrypt');
 const router = express.Router();
 const storage = multer.memoryStorage();
-
-// const storage = multer.diskStorage({
-//   destination: './public/avatars',
-//   filename: (req, file, cb) => {
-//     cb(null, `${new Date().getTime()}-${file.originalname}`);
-//   }
-// })
 
 const upload = multer({
   storage: storage,
@@ -23,7 +15,6 @@ router.put('/', upload.single('avatar'), async function (req, res) { // here
   const user = jwt.verify(token, process.env.JWT_KEY)
   const file = req.file;
   let imageURL;
-  // const imageURL = `http://localhost:3000/public/avatars/${file?.filename}`;
   console.log('post updated: ',req.body);
 
   try {
@@ -32,21 +23,21 @@ router.put('/', upload.single('avatar'), async function (req, res) { // here
     const compare = await bcrypt.compare(req.body.password, dbPassword);
 
     if(compare){
-      // const [image] = await req.db.query(` deleting image
-      //   SELECT avatar FROM users
-      //   WHERE id = ${user.userId}
-      // `)
 
-      // if (image[0].avatar) {
-      //   fs.unlink(image[0].avatar.split("/").splice(3, 6).join("/"), err => {
-      //     if (err) {
-      //       console.log("delete image error: ", err);
-      //     } else {
-      //       console.log("Image deleted")
-      //     }
-      //   })
-      // }
       if(file) {
+        const [image] = await req.db.query(` deleting image
+        SELECT avatar FROM users
+        WHERE id = ${user.userId}
+        `)
+        if (image[0].avatar) {
+          await del(image[0].avatar)
+          if (err) {
+            console.log("delete image error: ", err);
+          } else {
+            console.log("Image deleted")
+          }
+        }
+
         const blobName = `${new Date().getTime()}-${file.originalname}`
         const blob = await put(blobName, file.buffer, {
           access: 'public'
