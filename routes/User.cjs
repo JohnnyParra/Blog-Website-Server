37b56e6935ca10f5b60f5c17e517697864093b7a
Johnny Parra <juanparra1996@gmail.com>
2024-06-25 17:00:42 -0700

    Version 1.1

--- routes/User.cjs
+++ routes/User.cjs
@@ -19,52 +19,83 @@ try {
 });
 
 
-router.get('/posts/liked', async (req, res) => {
+router.get('/posts/liked/:page', async (req, res) => {
 const [scheme, token] = req.headers.authorization.split(' ');
-const user = jwt.verify(token, process.env.JWT_KEY)
-console.log('user: ', user)
+const user = jwt.verify(token, process.env.JWT_KEY);
+const page = Number(req.params.page) - 1;
+const nextPage = Number(req.params.page) + 1;
+const itemsPerPage = 10;
 
 try {
+  const [count] = await req.db.query(`
+    SELECT COUNT(*) as count FROM posts
+    WHERE posts.id IN(
+      SELECT post_id FROM post_likes
+      WHERE post_likes.user_id = ${user.userId}
+    )`
+  );
+  const hasMore = (page + 1) * itemsPerPage < count[0]['count'];
+
   const [posts] = await req.db.query(`
   SELECT * FROM posts
   WHERE posts.id IN(
     SELECT post_id FROM post_likes
     WHERE post_likes.user_id = ${user.userId}
   )
-  ORDER BY posts.date_published DESC`
+  ORDER BY posts.date_published DESC
+  LIMIT ${page * itemsPerPage}, ${itemsPerPage}`
 );
 
-  res.json({ posts });
+  res.json({ posts, count, hasMore, nextPage });
 } catch (err) {
   console.log(err);
   res.json({ err });
 }
 });
 
-router.get('/posts/:published', async (req, res) => {
+router.get('/posts/:published/:page', async (req, res) => {
   const [scheme, token] = req.headers.authorization.split(' ');
   const user = jwt.verify(token, process.env.JWT_KEY)
   const published = req.params.published;
-  console.log('user: ', user)
+  const page = Number(req.params.page) - 1;
+  const nextPage = Number(req.params.page) + 1;
+  const itemsPerPage = 10;
   
   try {
     if (published === 'deleted') {
-      const [posts] = await req.db.query(`
-      SELECT * FROM posts
-      WHERE user_id = ${user.userId} 
-        AND date_deleted is not NULL
-      ORDER BY date_published DESC`
+      const [count] = await req.db.query(`
+        SELECT COUNT(*) as count FROM posts p
+        WHERE p.user_id = ${user.userId}
+          AND p.date_deleted is not NULL`
       );
-      res.json({ posts });
+      const hasMore = (page + 1) * itemsPerPage < count[0]['count'];
+
+      const [posts] = await req.db.query(`
+        SELECT * FROM posts
+        WHERE user_id = ${user.userId} 
+          AND date_deleted is not NULL
+        ORDER BY date_published DESC
+        LIMIT ${page * itemsPerPage}, ${itemsPerPage}`
+      );
+      res.json({ posts, count, hasMore, nextPage });
     } else {
-      const [posts] = await req.db.query(`
-      SELECT * FROM posts
-      WHERE user_id = ${user.userId} 
-        AND is_published = ${published}
-        AND date_deleted is NULL
-      ORDER BY date_published DESC`
+      const [count] = await req.db.query(`
+        SELECT COUNT(*) as count FROM posts p
+        WHERE p.user_id = ${user.userId}
+          AND is_published = ${published}
+          AND p.date_deleted is NULL`
       );
-      res.json({ posts });
+      const hasMore = (page + 1) * itemsPerPage < count[0]['count'];
+
+      const [posts] = await req.db.query(`
+        SELECT * FROM posts
+        WHERE user_id = ${user.userId} 
+          AND is_published = ${published}
+          AND date_deleted is NULL
+        ORDER BY date_published DESC
+        LIMIT ${page * itemsPerPage}, ${itemsPerPage}`
+      );
+      res.json({ posts, count, hasMore, nextPage });
     }
   } catch (err) {
     console.log(err);
